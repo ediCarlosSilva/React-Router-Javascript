342fb573276fe310481d7809ced81ac454171d5e
Edi Carlos <edi_csilva@hotmail.com>
2023-04-18 23:20:10 +0000

    using NavLink

--- src/componentes/MenuLink/index.js
+++ src/componentes/MenuLink/index.js
@@ -1,16 +1,17 @@
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './MenuLink.module.css';
 
-export default function MenuLink({children, to}) {
+export default function MenuLink({ children, to }) {
 
-    const localizacao = useLocation();
-    
-    return (
-        <Link className={`
-                        ${styles.link}
-                        ${localizacao.pathname === to ? styles.linkDestacado : ''}
-                        `} to={to}>
-            {children}
-        </Link>
-    )
+  return (
+    <NavLink
+      to={to}
+      className={
+          ({ isActive }) => `
+            ${styles.link}
+            ${isActive ? styles.linkDestacado : ''}
+            `} end>
+      {children}
+    </NavLink>
+  )
 }
