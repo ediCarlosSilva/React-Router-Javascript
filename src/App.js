b4f77bc072889c5368e4664c8b12cb25c12bda73
Edi Carlos <edi_csilva@hotmail.com>
2023-04-18 13:58:33 +0000

    Using componente BrowserRouter da biblioteca react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
 import Inicio from "./paginas/inicio";
 import SobreMim from "./paginas/sobreMim";
 
-console.log(window.location);
-
-const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />;
-
 function App() {
-  return pagina;
+  return (
+    <BrowserRouter>
+      
+    </BrowserRouter>
+  );
 }
 
 export default App;
