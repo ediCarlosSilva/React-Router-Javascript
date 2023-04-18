9cbe43bd98f9e19c46d9d822d3d0e1abe94990cf
Edi Carlos <edi_csilva@hotmail.com>
2023-04-18 13:24:05 +0000

    Adicionado primeiras paginas. Primeiras lógias do controle de rotas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/sobreMim";
+
 console.log(window.location);
 
-const pagina = window.location.pathname === '/' ? <div>Olá Mundo!</div> : <div>Sobre mim...</div>
+const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />;
 
 function App() {
   return pagina;
